a4c62e21c1357210eeccefcff4ef6ab4a49af679
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-02 01:14:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # get all people, combine and uniq them
+  people = data.flat_map {|label, category|
+    category.flat_map {|value, names| names}
+  }.uniq
+  people.map {|name|
+    [name, {
+      :color => find_matches(data, :color, name),
+      :gender => find_matches(data, :gender, name),
+      :lives => find_matches(data, :lives, name)
+    }]
+  }.to_h
+end
+
+def find_matches data, key, name
+  data[key].reduce([]) {|matches, (label, people)|
+    matches << label.to_s if people.include? name
+    matches.uniq
+  }
 end
